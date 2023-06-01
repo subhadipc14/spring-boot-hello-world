ea0a9ac41ceecbe87f00de17e7f469f169a0f4ae
subhadipc14 <subhadipc14@gmail.com>
2023-06-01 09:44:41 +0530

    push version 1

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -3,11 +3,12 @@ package com.example.helloworld.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// This is sample controller file 
 @RestController
 public class HelloWorldController {
 
     @GetMapping("/hello")
     public String sendGreetings() {
-        return "Hello, World!";
+        return "Hello, World Again!";
     }
 }
