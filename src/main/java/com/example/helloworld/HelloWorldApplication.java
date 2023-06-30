02e82caabe1f5f291eb4e2b6964643f8edc13fb0
subhadipc14 <subhadipc14@gmail.com>
2023-06-30 09:36:13 +0530

    pushed azure devops start

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -2,12 +2,20 @@ package com.example.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class HelloWorldApplication {
+	
+	 @GetMapping("/")
+		public String home() {
+			return "Azure devops classes";
+		}
+
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
     }
-
+    
+   
 }
