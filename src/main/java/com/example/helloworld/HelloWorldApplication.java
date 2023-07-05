4f488ffbcb09a81d0b4a8a234bf7fd9f443bcf0c
subhadipc14 <132640697+subhadipc14@users.noreply.github.com>
2023-07-05 10:55:06 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@RestController
 public class HelloWorldApplication {
 	
 	 @GetMapping("/")
