e3cd72585f2aa3afd5332d6afb38b913a30a9bd2
subhadipc14 <subhadipc14@gmail.com>
2023-07-12 09:58:40 +0530

    pushed 12/7

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -3,6 +3,7 @@ package com.example.helloworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
